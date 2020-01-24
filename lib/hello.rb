5b2c066bb09617f8903c3d29a58b12fa2fe85f91
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 22:25:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,12 @@
 def hello_t(names)
-  i = 0
- 
-  while (i < names.length)
-    yield names[i]
-    i = i + 1
-  end
-  names
+  if block_given?
+    i = 0
+   
+    while (i < names.length)
+      yield names[i]
+      i = i + 1
+    end
+    names
 end
 
 # call your method here!
