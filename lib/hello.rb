4bdb1cc88720d08df23f90e38cb7bb284f7e64a0
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 22:23:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
 def hello_t(names)
-
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
 # call your method here!
 
+hello_t(["Thomas", "Ariane", "Ivy", "Thatan"]){ |name|
+  
+}
