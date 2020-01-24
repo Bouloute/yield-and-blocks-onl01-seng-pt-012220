fc16f69a779e6b424ddf75320bd59ea1350f1dc8
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 22:24:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,7 +9,7 @@ end
 
 # call your method here!
 
-hello_t(["Thomas", "Ariane", "Ivy", "Thatan"]){ |name|
+hello_t(["Tim", "Tom", "Jim"]){ |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
